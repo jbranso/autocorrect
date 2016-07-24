18ec727d2b0317a9ba09daef8980d54e0ea2e4b7
Joshua Branson <bransoj@hotmail.com>
2016-07-24 18:09:13 -0400

    I believe that I now have the auto-correct package working.  I may need to work on making sure that it doesn't inter fer-with helm mode.

--- autocorrect.el
+++ autocorrect.el
@@ -1,23 +1,51 @@
+(defvar autocorrect-regexp "^[a-zA-Z]+[;.,?!)\"]\\{0,3\\}$"
+  "autocorrect-regexp is the regexp that makes sure the word is a word that we want to try to correct.
+For example, my package should not try to correct a variable name.  So any word with any special
+symbols in the middle of it or numbers, should not be corrected.")
+
+(defun autocorrect/word-is-correctable ()
+"This function makes sure that we are not calling ispell on words that are not meant to be corrected.  Like 'variable2'."
+(interactive))
+
+(defun reg-test/previous-word ()
+  "Returns the previous word in the buffer."
+  (interactive)
+  (let (reg-test/previousWord)
+    (setq reg-test/previousWord (progn
+                                  (backward-word)
+                                  (word-at-point)))
+    (forward-word)
+    (forward-char)
+    reg-test/previousWord))
+
+
+
 (defun my/flyspell-auto-correct-word ()
   "If the last entered character is SPC, then run flyspell-auto-correct-word on the last word "
   (interactive)
-  (let (previousChar nextChar)
+  (let (previousWord previousChar nextChar current-point)
+    (setq current-point (point))
+    (setq previousWord (reg-test/previous-word))
     ;; get the char before point.  If you have just pressed the space bar, then the char before point is SPC.
     ;; if you have just pressed "h", then the char before point is "h".
     (setq previousChar
           (substring (buffer-substring (- (point) 1) (point)) 0))
     (setq nextChar
-          (substring (buffer-substring (point) (+ 1 (point))) 0))
-    (when (string= previousChar " ")
-      (flyspell-auto-correct-word)
-      (if (string= nextChar "\n")
-          (insert " ")
-        (forward-char)))))
+          (substring (buffer-substring (point) (+ 1 t(point))) 0))
+    (when (and (string= previousChar " ")
+               ;; the string should just be alphanumeric characters, or it might have punctuation at the end.  Like "Hello?"
+               ;; (additional details)
+               ;; "I am a sentence," said me.
+               ;; "I don't care what you think," said Sally, "but if you would like, I can punch you in the face."
+               (string-match autocorrect-regexp previousWord))
+      (progn
+        (flyspell-auto-correct-word)))
+    (goto-char current-point)))
 
 (defun add-my-flyspell-auto-correct-word-hook ()
   "This function adds my/flyspell-auto-correct-word function to be run after post-self-insert-hook."
   (interactive)
-  (add-hook 'post-self-insert-hook 'my/flyspell-auto-correct-word))
+  (add-hook 'post-self-insert-hook 'my/flyspell-auto-correct-wordt))
 
 (defun remove-my-flyspell-auto-correct-word-hook ()
   "This function adds my/flyspell-auto-correct-word function to be run after post-self-insert-hook."
--- regression-testing.el
+++ regression-testing.el
@@ -1,74 +1,94 @@
 (defun reg-test/go-to-before (reg-test/buffer)
   "go to the before line."
   (interactive)
-  (find-file (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
-  (beginning-of-buffer)
-  (search-forward "* before")
-  (next-line)
-  (beginning-of-line)
-  (forward-char 3))
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (with-current-buffer reg-test/buffer
+    (beginning-of-buffer)
+    (search-forward "* before")
+    (next-line)
+    (beginning-of-line)
+    (forward-char 3)
+    (let (reg-test/point)
+      (setq reg-test/point
+            (point))
+      reg-test/point)))
 
 (defun reg-test/go-to-after (reg-test/buffer)
   "go to the before line."
   (interactive)
-  (find-file (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
-  (beginning-of-buffer)
-  (search-forward "* after")
-  (next-line)
-  (beginning-of-line)
-  (forward-char 3)
-  )
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (with-current-buffer reg-test/buffer
+    (beginning-of-buffer)
+    (search-forward "* after")
+    (next-line)
+    (beginning-of-line)
+    (forward-char 3)
+    (let (reg-test/point)
+      (setq reg-test/point (point))
+      reg-test/point)))
 
 (defun reg-test/get-before-line-number (reg-test/buffer)
   "This returns the line number at the before heading."
-  (reg-test/go-to-before reg-test/buffer)
-  (line-number-at-pos))
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (let (reg-test/line-number)
+    (with-current-buffer reg-test/buffer
+      (goto-char (reg-test/go-to-before reg-test/buffer))
+      (setq reg-test/line-number
+            (line-number-at-pos)))
+    reg-test/line-number)
+  )
 
 (defun reg-test/get-after-line-number (reg-test/buffer)
   "This returns the line number at the before heading."
-  (reg-test/go-to-after reg-test/buffer)
-  (line-number-at-pos))
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (let (reg-test/line-number)
+    (with-current-buffer reg-test/buffer
+      (goto-char (reg-test/go-to-after reg-test/buffer))
+      (setq reg-test/line-number
+            (line-number-at-pos)))
+    reg-test/line-number))
 
 (require 'ert)
 (defun reg-test/get-before (reg-test/buffer)
   "This returns the before text."
   (interactive)
-  (reg-test/go-to-before reg-test/buffer)
-  (let (string-before)
-    (setq string-before (buffer-substring (point) (progn
-                                                    (search-forward-regexp "$")
-                                                    (point))))
-    string-before))
+  ;;(reg-test/go-to-before reg-test/buffer)
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (with-current-buffer reg-test/buffer
+    (goto-char (reg-test/go-to-before reg-test/buffer))
+    (buffer-substring (point) (progn
+                                (search-forward-regexp "$")
+                                (point)))))
 
 (defun reg-test/get-after (reg-test/buffer)
   "This returns the before text."
   (interactive)
-  (reg-test/go-to-after reg-test/buffer)
-  (let (string-after)
-    (setq string-after
-          (buffer-substring (point) (progn
-                                      (search-forward-regexp "$")
-                                      (point))))
-    string-after))
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (with-current-buffer reg-test/buffer
+    (goto-char (reg-test/go-to-after reg-test/buffer))
+    (buffer-substring (point) (progn
+                                (search-forward-regexp "$")
+                                (point)))))
 
 (defun reg-test/correct-all-words-on-current-line (line reg-test/buffer)
   "This function will move to each word on the line, and press the space bar, which will call my/flyspell-auto-correct-word."
   (interactive)
   (forward-word)
   ;; I am now at the end of a word
-  (if (equal line (line-number-at-pos))
-      (progn
-        (insert " ")
-        (my/flyspell-auto-correct-word)
-        (reg-test/correct-all-words-on-current-line line reg-test/buffer))
-    t))
+  (when (equal line (line-number-at-pos))
+    (insert " ")
+    (my/flyspell-auto-correct-word)
+    (reg-test/correct-all-words-on-current-line line reg-test/buffer)))
 
 (defun reg-test/autocorrect-words-are-correct (reg-test/buffer)
   "This function runs the test specifies in ./tests/words-are-correct.org"
   (interactive)
-  (reg-test/correct-all-words-on-current-line
-   (reg-test/get-before-line-number reg-test/buffer)
-   reg-test/buffer))
+  (find-file-noselect (concat "~/programming/emacs/autocorrect/tests/" reg-test/buffer))
+  (with-current-buffer reg-test/buffer
+    (goto-char (reg-test/go-to-before reg-test/buffer))
+    (reg-test/correct-all-words-on-current-line
+     (reg-test/get-before-line-number reg-test/buffer)
+     reg-test/buffer)))
 
 (ert-deftest autocorrect-words-are-correct ()
   (let (reg-test/buffer)
@@ -78,25 +98,4 @@
               (setq stringEqual
                     (string= (reg-test/get-before reg-test/buffer) (reg-test/get-after reg-test/buffer)))
               (kill-buffer reg-test/buffer)
-              stringEqual)))
-  )
-
-
-(defun thisShouldReturnT ()
-  ""
-  (interactive)
-  (let (reg-test/buffer)
-    (setq reg-test/buffer "words-are-correct.org")
-    (reg-test/autocorrect-words-are-correct reg-test/buffer)
-    (let (stringEqual)
-      (setq stringEqual
-            (string= (reg-test/get-before reg-test/buffer) (reg-test/get-after reg-test/buffer)))
-      (kill-buffer reg-test/buffer)
-      stringEqual))) 
-
-;;      (thisShouldReturnT)
-
-
-(reg-test/autocorrect-words-are-correct "words-are-correct.org")
-(setq stringEqual
-      (string= (reg-test/get-before "words-are-correct.org") (reg-test/get-after "words-are-correct.org")))
+              stringEqual))))
