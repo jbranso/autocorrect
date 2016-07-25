bf1a688e25bafe89054755915b7bb1921e553101
Joshua Branson <bransoj@hotmail.com>
2016-07-25 15:51:05 -0400

    I am trying to make autocorrect work with org-mode.  It should turn off and on if the user is inside an org babel src block.  But right now what I've done has broken just about everything.  I'll fix it later.

--- autocorrect.el
+++ autocorrect.el
@@ -3,22 +3,16 @@
 For example, my package should not try to correct a variable name.  So any word with any special
 symbols in the middle of it or numbers, should not be corrected.")
 
-(defun autocorrect/word-is-correctable ()
-"This function makes sure that we are not calling ispell on words that are not meant to be corrected.  Like 'variable2'."
-(interactive))
-
 (defun reg-test/previous-word ()
   "Returns the previous word in the buffer."
   (interactive)
-  (let (reg-test/previousWord)
-    (setq reg-test/previousWord (progn
-                                  (backward-word)
-                                  (word-at-point)))
-    (forward-word)
-    (forward-char)
-    reg-test/previousWord))
-
-
+  (let (reg-test/previous-word current-point)
+    (setq current-point (point))
+    (setq reg-test/previous-word (progn
+                                   (backward-word)
+                                   (word-at-point)))
+    (goto-char current-point)
+    reg-test/previous-word))
 
 (defun my/flyspell-auto-correct-word ()
   "If the last entered character is SPC, then run flyspell-auto-correct-word on the last word "
@@ -31,25 +25,41 @@ symbols in the middle of it or numbers, should not be corrected.")
     (setq previousChar
           (substring (buffer-substring (- (point) 1) (point)) 0))
     (setq nextChar
-          (substring (buffer-substring (point) (+ 1 t(point))) 0))
+          (substring (buffer-substring (point) (+ 1 (point))) 0))
     (when (and (string= previousChar " ")
                ;; the string should just be alphanumeric characters, or it might have punctuation at the end.  Like "Hello?"
                ;; (additional details)
                ;; "I am a sentence," said me.
                ;; "I don't care what you think," said Sally, "but if you would like, I can punch you in the face."
-               (string-match autocorrect-regexp previousWord))
+               (string-match autocorrect-regexp previousWord ))
       (progn
         (flyspell-auto-correct-word)))
     (goto-char current-point)))
 
-(defun add-my-flyspell-auto-correct-word-hook ()
+(defun add-autocorrect-hook ()
   "This function adds my/flyspell-auto-correct-word function to be run after post-self-insert-hook."
   (interactive)
-  (add-hook 'post-self-insert-hook 'my/flyspell-auto-correct-wordt))
+  (add-hook 'post-self-insert-hook 'my/flyspell-auto-correct-word))
 
-(defun remove-my-flyspell-auto-correct-word-hook ()
+(defun remove-autocorrect-hook ()
   "This function adds my/flyspell-auto-correct-word function to be run after post-self-insert-hook."
   (interactive)
   (remove-hook 'post-self-insert-hook 'my/flyspell-auto-correct-word))
 
+(add-hook 'minibuffer-inactive-mode-hook 'remove-autocorrect-hook)
+
+(add-hook 'text-mode-hook #'add-autocorrect-hook)
+(add-hook 'programming-mode-hook #'remove-autocorrect-hook)
+
 (provide 'init-autocorrect)
+
+(defun autocorrect-mode-is-a-prog-mode ())
+
+
+(string= "Parent mode: `prog-mode"
+         (substring (describe-function 'emacs-lisp-mode)
+                    (search "Parent mode:"
+                            (describe-function 'emacs-lisp-mode))
+                    119))
+(delete-window
+ (get-buffer-window "*Help*"))
